fa8e856ba2d4e78adab891843f54c7e7ea548c9b
sushi057 <suvash2077@gmail.com>
2024-10-02 12:27:52 +0545

    fix: remove planner agent

--- agents.py
+++ agents.py
@@ -5,34 +5,47 @@ from pydantic import BaseModel, Field
 from langchain_openai import ChatOpenAI
 
 from state import AgentGraphState, get_agent_graph_state
+from prompts import (
+    planner_prompt_template,
+    retriever_prompt_template,
+    reviewer_prompt_template,
+    quality_control_prompt_template,
+    final_report_prompt_template,
+)
 
 
 class Agent:
     def __init__(self, state: AgentGraphState, model=None, temperature=None):
         self.state = state
         self.llm = ChatOpenAI(
-            model=model, temperature=temperature, api_key=os.getenv("OPENAI_API_KEY")
+            model="gpt-4o-mini", temperature=0.5, api_key=os.getenv("OPENAI_API_KEY")
         )
-
-    def update_state(self, key, value):
-        state = {**self.state, key: value}
 
 
 class PlannerAgent(Agent):
     def invoke(self, next_section=None):
-        self.update_state("current_section", next_section)
-        # self.update_status("user_prompt", user_prompt)
+        self.state = {**self.state, "next_section": next_section}
+
         return self.state
 
 
 class RetrievalAgent(Agent):
     def invoke(self):
-        # self.update_state("messages
+        retriever_runnable = retriever_prompt_template | self.llm
+        response = retriever_runnable.invoke(self.state)
+
+        self.state = {**self.state, "messages": response}
+        self.state = {**self.state, "current_section_text": response.content}
         return self.state
 
 
 class ReviewerAgent(Agent):
     def invoke(self):
+        reviewer_runnable = reviewer_prompt_template | self.llm
+        response = reviewer_runnable.invoke(self.state)
+
+        self.state = {**self.state, "messages": response}
+        self.state = {**self.state, "current_section_text": response.content}
         return self.state
 
 
--- graph.py
+++ graph.py
@@ -2,11 +2,55 @@ import os
 
 from langgraph.graph import START, END, StateGraph
 from langchain_openai import ChatOpenAI
+from langgraph.checkpoint.memory import MemorySaver
 
-from state import State
+from state import AgentGraphState, get_agent_graph_state
+from agents import (
+    RetrievalAgent,
+    ReviewerAgent,
+    QualityControlAgent,
+    FinalReportAgent,
+)
 
 
-graph_builder = StateGraph(State)
+def create_graph():
+    graph_builder = StateGraph(AgentGraphState)
 
-llm = ChatOpenAI(model="gpt-4o", api_key=os.getenv("OPENAI_API_KEY"))
+    # graph_builder.add_node(
+    #     "planner_agent",
+    #     lambda state: PlannerAgent(
+    #         state,
+    #     ).invoke(),
+    # )
 
+    graph_builder.add_node(
+        "reviewer_agent", lambda state: ReviewerAgent(state).invoke()
+    )
+
+    graph_builder.add_node(
+        "retriever_agent", lambda state: RetrievalAgent(state).invoke()
+    )
+
+    graph_builder.add_node(
+        "quality_control_agent",
+        lambda state: QualityControlAgent(
+            state,
+        ).invoke(),
+    )
+
+    graph_builder.add_node(
+        "final_report_agent", lambda state: FinalReportAgent(state).invoke()
+    )
+
+    # graph_builder.add_edge(START, "planner_agent")
+    graph_builder.add_edge(START, "retriever_agent")
+    graph_builder.add_edge("retriever_agent", "reviewer_agent")
+    graph_builder.add_edge("reviewer_agent", "quality_control_agent")
+    graph_builder.add_edge("quality_control_agent", "final_report_agent")
+    graph_builder.add_edge("final_report_agent", END)
+
+    memory = MemorySaver()
+    graph = graph_builder.compile(
+        checkpointer=memory,
+    )
+    return graph
