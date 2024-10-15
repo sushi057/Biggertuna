b83f70c317f4cca4123ba9e554f32ba2cf43d134
sushi057 <suvash2077@gmail.com>
2024-10-15 10:35:32 +0545

    feat: add chainlit

---
+++ app.py
@@ -0,0 +1,59 @@
+import uuid
+import chainlit as cl
+from graph import create_graph
+
+thread_id = str(uuid.uuid4())
+
+graph = create_graph()
+
+config = {"configurable": {"thread_id": thread_id}}
+
+#  Plan report outline
+report_sections = [
+    "title, field_of_invention, background",
+    "reference_numbers",
+    "brief_description",
+    "detailed_description",
+    "claims",
+    "glossary_of_terms",
+    "abstract",
+    "other_embodiments",
+]
+
+# user_prompt = "review the attachments and write a PPA for the idea of a tilt sensor that attaches to wrestling headgear to help train wrestlers to keep their head up.the sensor will attach to the straps of the headgear, have adjustability to set the zero and setpoint positions, and have some kind of auditory and visual and tactile feedback, like vibration."
+
+
+@cl.on_chat_start
+async def on_chat_start():
+    initial_prompt = await cl.AskUserMessage(
+        "Enter initial prompt: ", timeout=30
+    ).send()
+    for event in graph.stream(
+        {
+            "messages": {"user": initial_prompt["content"]},
+            "current_section": report_sections,
+        }
+    ):
+        try:
+            await cl.Message(content=event["messages"][-1].content).send()
+        except Exception as e:
+            cl.Message(content="An error occurred: " + str(e)).send()
+
+
+@cl.on_message
+async def on_message(message: cl.Message):
+    # user_input = message.
+    graph.update_state(
+        config, {"messages": [("user", message)]}, as_node="feedback_agent"
+    )
+
+    messages = []
+    for event in graph.stream(
+        None,
+        config,
+        stream_mode="values",
+    ):
+        # for value in event.values():
+        # print("Assistant:", value["messages"].content, "\n")
+        event["messages"][-1].pretty_print()
+    await cl.Message(content=event["messages"][-1].content).send()
